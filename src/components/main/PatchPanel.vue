ce07cf37414c0ee273877589637073deb806bef1
Inf Sein <Micro-cn-diss@hotmail.com>
2024-05-20 17:20:44 +0800

    feat: use `./` instead of superfluous `~ApiBase`

--- src/components/main/PatchPanel.vue
+++ src/components/main/PatchPanel.vue
@@ -42,12 +42,10 @@ const getButtonSize = () => {
 
 const buildButtonStyle = (patch: any) => {
   const size = getButtonSize()
-  const logo = patch.logo.replace('~ApiBase', '/hqhelper-dawntrail')
-  const logo_alt = patch.logo.replace('~ApiBase', '')
   return [
     `width: ${size.width}px;`,
     `height: ${size.height}px;`,
-    `background-image: url(${logo}), url(${logo_alt});`
+    `background-image: url(${patch.logo});`
   ].join(' ')
 }
 const buildButtonContentStyle = () => {
