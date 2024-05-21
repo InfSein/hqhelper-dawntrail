9de418f1e2bb739fce0f43ce69536d56481f6708
Inf Sein <Micro-cn-diss@hotmail.com>
2024-05-21 18:20:52 +0800

    feat: add a description popup for `version` in `app-header`

--- src/components/main/AppHeader.vue
+++ src/components/main/AppHeader.vue
@@ -6,8 +6,8 @@ import {
   SettingsSharp,
   InfoFilled
 } from '@vicons/material'
-import EorzeaTime from '@/tools/EorzeaTime'
-import AppStatus from '@/variables/AppStatus'
+import EorzeaTime from '@/tools/eorzea-time'
+import AppStatus from '@/variables/app-status'
 
 const t = inject<(text: string, ...args: any[]) => string>('t') ?? (() => { return '' })
 const showUserPreferencesModal = inject<() => void>('showUserPreferencesModal') ?? (() => {})
@@ -47,8 +47,19 @@ const openModal = (key: string) => {
   <div class="app-header">
     <i class="xiv hq logo-font"></i>
     <p class="app-name">HQ Helper</p>
-    <p>{{ AppStatus.Version }}</p>
+    <n-popover trigger="hover" style="max-width: 260px;">
+      <template #trigger>
+        <p>{{ AppStatus.Version }}</p>
+      </template>
+      <div class="flex-column">
+        <p>{{ t('国服数据版本：{}', AppStatus.SupportedGameVersion.CN) }}</p>
+        <p>{{ t('国际服数据版本：{}', AppStatus.SupportedGameVersion.GLOBAL) }}</p>
+        <p>{{ t('※ 国服数据尚未更新时，显示的中文名一般为作者自译，请谨慎参考。') }}</p>
+      </div>
+    </n-popover>
+
     <n-divider vertical></n-divider>
+
     <n-popover trigger="hover">
       <template #trigger>
         <p>
@@ -61,6 +72,7 @@ const openModal = (key: string) => {
         <p class="font-center">{{ t('艾欧泽亚时间') }}</p>
       </div>
     </n-popover>
+
     <div class="tail-contents">
       <n-button ghost class="menu-button" @click="showMenus = !showMenus">
         <template #icon>
