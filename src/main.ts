2fa9956b6120fe60ed1700e425fba5a04f8ea21a
InfSein <infsein@foxmail.com>
2024-05-30 19:45:02 +0800

    fix: import `NButtonGroup`

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ import {
   // components
   // use a-z sort
   NAlert,
-  NButton, NBackTop,
+  NButton, NButtonGroup, NBackTop,
   NCard, NConfigProvider, NCollapse, NCollapseItem,
   NDrawer, NDrawerContent, NDropdown, NDialog, NDialogProvider, NDivider,
   NFlex, NForm, NFormItem,
@@ -29,7 +29,7 @@ import {
 const naive = create({
   components: [
     NAlert,
-    NButton, NBackTop,
+    NButton, NButtonGroup, NBackTop,
     NCard, NConfigProvider, NCollapse, NCollapseItem,
     NDrawer, NDrawerContent, NDropdown, NDialog, NDialogProvider, NDivider,
     NFlex, NForm, NFormItem,
