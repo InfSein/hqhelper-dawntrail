b9efbac7740dc4ac9d2df86759a3f5069d3aea86
InfSein <infsein@foxmail.com>
2024-06-25 20:30:39 +0800

    feat: try to optimize CHANGELOG display.

--- src/models/changelog.ts
+++ src/models/changelog.ts
@@ -7,6 +7,8 @@ export interface UpdateLog {
   date: string;
   /** 更新内容 */
   content: Record<string, string[]>;
+  /** 与之前的版本之间的更新日志是否不连贯，这一般出现在早期开发阶段中 */
+  discontinuous: boolean;
 }
 
 /** 将`CHANGELOG.md`的内容转换为`UpdateLog[]`格式的对象 */
@@ -24,8 +26,8 @@ export const convertToUpdateLogs = (changelog: string) => {
       }
       const version = versionMatch[1]
       const date = versionMatch[2]
-      const type = getType(previousVersion || '0.0.0', version)
-      currentLog = { version, type, date, content: {} }
+      const { type, discontinuous } = getType(previousVersion || '0.0.0', version)
+      currentLog = { version, type, date, content: {}, discontinuous }
       previousVersion = version
     } else if (currentLog) {
       const sectionMatch = line.match(/^###\s+(.*)/)
@@ -50,15 +52,27 @@ export const convertToUpdateLogs = (changelog: string) => {
   return updateLogs
 }
 
-function getType(previousVersion: string, currentVersion: string): "major" | "minor" | "patch" {
-  const [prevMajor, prevMinor/*, prevPatch*/] = previousVersion.split('.').map(Number)
-  const [currMajor, currMinor/*, currPatch*/] = currentVersion.split('.').map(Number)
+/** 格式不标准的版本号，这些版本号将被认定为大型版本更新 */
+const majorVersions = [
+  '项目成立', 'Project Established', 'プロジェクト開発開始'
+]
+function getType(previousVersion: string, currentVersion: string) {
+  let type: "major" | "minor" | "patch" = 'patch', discontinuous = false
 
-  if (currMajor > prevMajor) {
-    return "major"
-  } else if (currMinor > prevMinor) {
-    return "minor"
-  } else {
-    return "patch"
+  if (majorVersions.includes(previousVersion)) {
+    discontinuous = true
   }
+  if (majorVersions.includes(currentVersion)) {
+    type = "major"
+  } else {
+    const [prevMajor, prevMinor/*, prevPatch*/] = previousVersion.split('.').map(Number)
+    const [currMajor, currMinor/*, currPatch*/] = currentVersion.split('.').map(Number)
+    if (currMajor > prevMajor) {
+      type = "major"
+    } else if (currMinor > prevMinor) {
+      type = "minor"
+    }
+  }
+
+  return { type, discontinuous }
 }
