33caa0074c7224befd28616209960161c875a676
Inf Sein <Micro-cn-diss@hotmail.com>
2024-07-24 16:07:38 +0800

    feat: 添加路由页面 `ErrorPage404`

---
+++ src/views/ErrorPage404.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex-center w-full h-full">
+    <n-result status="404" title="404 Not Found">
+      <div>
+        <p v-for="(description, index) in descriptions" :key="index">{{ description }}</p>
+      </div>
+      <template #footer>
+        <div class="footer-container">
+          <n-button @click="$router.go(-1)">返回上一页</n-button>
+          <n-button @click="$router.push('/')">返回首页</n-button>
+        </div>
+      </template>
+    </n-result>
+  </div>
+</template>
+
+<script setup lang="ts" name="ErrorPage404">
+import { computed, inject } from 'vue'
+import {
+  NButton, NResult
+} from 'naive-ui'
+
+const t = inject<(text: string, ...args: any[]) => string>('t') ?? (() => { return '' })
+
+const descriptions = computed(() => {
+  return [
+    t('访问的页面不存在。'),
+    t('请尝试回到上一页或直接返回首页。')
+  ]
+})
+</script>
+
+<style scoped>
+.footer-container {
+  display: flex;
+  justify-content: center;
+  
+  .n-button {
+    margin: 5px;
+  }
+}
+</style>
