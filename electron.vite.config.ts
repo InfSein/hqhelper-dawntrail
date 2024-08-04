1234236a503cc02b41665d6828963ed4ea973a09
Inf Sein <Micro-cn-diss@hotmail.com>
2024-08-04 18:02:23 +0800

    temp-commit: v1 of `electron-vite`

---
+++ electron.vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'electron-vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  main: {
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'electron/main/index.ts')
+        }
+      }
+    }
+  },
+  preload: {
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'electron/preload/index.ts')
+        }
+      }
+    }
+  },
+  renderer: {
+    root: '.',
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'index.html')
+        }
+      }
+    }
+  }
+})
