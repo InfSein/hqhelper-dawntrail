5ef1d6cc00c2c336d356e477499459a8a715b857
Inf Sein <Micro-cn-diss@hotmail.com>
2024-08-10 17:47:41 +0800

    temp-commit: new app-header

--- src/components/custom-controls/AppHeader.vue
+++ src/components/custom-controls/AppHeader.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { computed, inject, ref, type Ref } from 'vue'
 import {
-  NButton, NDrawer, NDrawerContent, NDivider, NFlex, NIcon, NPopover,
+  NButton, NButtonGroup, NDrawer, NDrawerContent, NDivider, NFlex, NIcon, NPopover,
   useMessage
 } from 'naive-ui'
 import {
+  ArrowCircleLeftOutlined,
   FastfoodOutlined,
   MenuFilled,
   SettingsSharp,
@@ -21,6 +22,7 @@ import AppStatus from '@/variables/app-status'
 import router from '@/router'
 
 const t = inject<(text: string, ...args: any[]) => string>('t') ?? (() => { return '' })
+const isMobile = inject<Ref<boolean>>('isMobile') ?? ref(false)
 const locale = inject<Ref<"zh" | "en" | "ja">>('locale') ?? ref('zh')
 const isChina = computed(() => locale.value === 'zh')
 const appForceUpdate = inject<() => {}>('appForceUpdate') ?? (() => {})
@@ -34,6 +36,14 @@ const showUserPreferencesModal = ref(false)
 const showAboutAppModal = ref(false)
 const showContactModal = ref(false)
 const showChangeLogsModal = ref(false)
+
+const canRouteBack = computed(() => {
+  return router.currentRoute.value.path !== '/'
+})
+const handleRouteBack = () => {
+  router.go(-1)
+}
+
 const displayUserPreferencesModal = () => {
   showUserPreferencesModal.value = true
 }
@@ -50,15 +60,21 @@ const redirectToFoodAndTincPage = () => {
   router.push('/fthelper')
 }
 
+interface MenuItem {
+  label: string
+  icon: any
+  hide?: boolean
+  click?: () => void
+}
 const menuItems = computed(() => {
   const hideFTHelper = router.currentRoute.value.path.startsWith('/fthelper')
-  return [
-    { key: 'ft-helper', label: t('食药计算'), hide: hideFTHelper, icon: FastfoodOutlined, click: redirectToFoodAndTincPage },
-    { key: 'contact-us', label: t('联系我们'), icon: ContactlessSharp, click: displayContactModal },
-    { key: 'change-logs', label: t('更新日志'), hide: true, icon: EventNoteFilled, click: displayChangeLogsModal },
-    { key: 'user-preferences', label: t('偏好设置'), icon: SettingsSharp, click: displayUserPreferencesModal },
-    { key: 'about-app', label: t('关于本作'), icon: InfoFilled, click: displayAboutAppModal },
-  ]
+  return {
+    ftHelper: { label: t('食药计算'), hide: hideFTHelper, icon: FastfoodOutlined, click: redirectToFoodAndTincPage } as MenuItem,
+    contact: { label: t('联系我们'), icon: ContactlessSharp, click: displayContactModal } as MenuItem,
+    changelogs: { label: t('更新日志'), hide: true, icon: EventNoteFilled, click: displayChangeLogsModal } as MenuItem,
+    userPreferences: { label: t('偏好设置'), icon: SettingsSharp, click: displayUserPreferencesModal } as MenuItem,
+    aboutApp: { label: t('关于本作'), icon: InfoFilled, click: displayAboutAppModal } as MenuItem
+  }
 })
 
 const openModal = (click?: (() => void)) => {
@@ -78,40 +94,67 @@ const onUserPreferencesSubmitted = () => {
 
 <template>
   <div class="app-header">
-    <i class="xiv hq logo-font"></i>
-    <p class="app-name">HQ Helper</p>
-    <n-popover trigger="hover" style="max-width: 260px;">
-      <template #trigger>
-        <p>{{ AppStatus.Version }}</p>
-      </template>
-      <div class="flex-column">
-        <p>{{ t('国服数据版本：{}', AppStatus.SupportedGameVersion.CN) }}</p>
-        <p>{{ t('国际服数据版本：{}', AppStatus.SupportedGameVersion.GLOBAL) }}</p>
-        <p>{{ t('※ 国服数据尚未更新时，显示的中文名一般为人工临时翻译，请谨慎参考。') }}</p>
-      </div>
-    </n-popover>
-
-    <n-divider vertical></n-divider>
-
-    <n-popover trigger="hover">
-      <template #trigger>
-        <p>
-          <span v-if="isChina"><i class="xiv eorzea-time-chs"></i></span>
-          <span v-else><i class="xiv eorzea-time"></i></span>
-          <span class="time-text">{{ currentET.gameTime }}</span>
-        </p>
-      </template>
-      <div class="flex-column flex-center">
-        <p class="font-center">{{ t('艾欧泽亚时间') }}</p>
-      </div>
-    </n-popover>
-
-    <div class="tail-contents">
-      <n-button ghost class="menu-button" @click="showMenus = !showMenus">
-        <template #icon>
-          <n-icon><menu-filled /></n-icon>
-        </template>
+    <div class="router-back-container" v-if="!isMobile">
+      <n-button text style="font-size: 35px" :disabled="!canRouteBack" @click="handleRouteBack">
+        <n-icon>
+          <ArrowCircleLeftOutlined />
+        </n-icon>
       </n-button>
+    </div>
+    <div class="header-content">
+      <div class="app-info">
+        <i class="xiv hq logo-font"></i>
+        <p class="app-name">HQ Helper</p>
+        <n-popover trigger="hover" style="max-width: 260px;">
+          <template #trigger>
+            <p>{{ AppStatus.Version }}</p>
+          </template>
+          <div class="flex-column">
+            <p>{{ t('国服数据版本：{}', AppStatus.SupportedGameVersion.CN) }}</p>
+            <p>{{ t('国际服数据版本：{}', AppStatus.SupportedGameVersion.GLOBAL) }}</p>
+            <p>{{ t('※ 国服数据尚未更新时，显示的中文名一般为人工临时翻译，请谨慎参考。') }}</p>
+          </div>
+        </n-popover>
+
+        <n-divider vertical></n-divider>
+
+        <n-popover trigger="hover">
+          <template #trigger>
+            <p>
+              <span v-if="isChina"><i class="xiv eorzea-time-chs"></i></span>
+              <span v-else><i class="xiv eorzea-time"></i></span>
+              <span class="time-text">{{ currentET.gameTime }}</span>
+            </p>
+          </template>
+          <div class="flex-column flex-center">
+            <p class="font-center">{{ t('艾欧泽亚时间') }}</p>
+          </div>
+        </n-popover>
+
+        <div class="tail-contents" v-if="isMobile">
+          <n-button ghost class="menu-button" @click="showMenus = !showMenus">
+            <template #icon>
+              <n-icon><menu-filled /></n-icon>
+            </template>
+          </n-button>
+        </div>
+      </div>
+      <div class="app-menu" v-if="!isMobile">
+        <n-button-group>
+          <n-button
+            size="tiny" tertiary
+            v-for="(item, key) in menuItems"
+            :key="key"
+            v-show="!item?.hide"
+            @click="openModal(item.click)"
+          >
+            <template #icon>
+              <n-icon><component :is="item.icon" /></n-icon>
+            </template>
+            {{ item.label }}
+          </n-button>
+        </n-button-group>
+      </div>
     </div>
     
     <n-drawer
@@ -125,8 +168,8 @@ const onUserPreferencesSubmitted = () => {
       <n-drawer-content>
         <n-flex vertical>
           <n-button
-            v-for="item in menuItems"
-            :key="item.key"
+            v-for="(item, key) in menuItems"
+            :key="key"
             v-show="!item?.hide"
             @click="openModal(item.click)"
           >
@@ -150,30 +193,70 @@ const onUserPreferencesSubmitted = () => {
 </template>
 
 <style scoped>
+/* All */
 .app-header {
+  height: 100%;
   display: flex;
   align-items: center;
-  line-height: 35px;
+  gap: 10px;
+  user-select: none;
 
-  .logo-font {
-    font-size: 24px;
-    color: var(--n-text-color);
-  }
-  .app-name {
-    margin: 5px;
-    font-weight: 600;
-  }
-  .time-text {
-    margin-left: 5px;
-    font-weight: 400;
-  }
-  .tail-contents {
-    margin-left: auto;
+  .router-back-container {
+    height: 100%;
     display: flex;
+    align-items: center;
+  }
 
-    .menu-button {
-      margin-left: 20px;
+  .header-content .app-info {
+    display: flex;
+    align-items: center;
+
+    .logo-font {
+      color: var(--n-text-color);
     }
+    .app-name {
+      margin: 5px;
+      font-weight: 600;
+    }
+    .time-text {
+      margin-left: 5px;
+      font-weight: 400;
+    }
+    .tail-contents {
+      margin-left: auto;
+      display: flex;
+
+      .menu-button {
+        margin-left: 20px;
+      }
+    }
+  }
+}
+
+/* Desktop */
+@media screen and (min-width: 768px) {
+  .app-header .header-content {
+    .app-info, .app-menu {
+      line-height: 1;
+    }
+  }
+  .app-menu {
+    display: flex;
+    gap: 10px;
+  }
+}
+
+/* Mobile */
+@media screen and (max-width: 767px) {
+  .app-header .header-content .app-info {
+    height: 100%;
+
+    .logo-font {
+      font-size: 24px;
+    }
+  }
+  .header-content, .app-info {
+    width: 100%;
   }
 }
 </style>
