b85061354d59fa36aeb98730a5ff245b65f4c7e9
Inf Sein <Micro-cn-diss@hotmail.com>
2024-08-21 16:45:00 +0800

    temp-commit: 适应electron

--- env.d.ts
+++ env.d.ts
@@ -5,3 +5,20 @@ declare module '*.vue' {
   const componentOptions: ComponentOptions
   export default componentOptions
 }
+
+// 宣言 `window` 的属性以允许类型检查
+declare global {
+  interface Window {
+    // * Electron API
+    electronAPI: undefined | {
+      checkForUpdates: () => void;
+      installUpdate: () => void;
+      onUpdateReady: (callBack: Function) => void;
+      minimize: () => void;
+      maximize: () => void;
+      restore: () => void;
+      close: () => void;
+      version: string;
+    };
+  }
+}
