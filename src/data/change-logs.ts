f88f14911deefc68fa14f309452d046559b0165a
Inf Sein <Micro-cn-diss@hotmail.com>
2024-09-09 17:55:02 +0800

    temp-commit: changelog

--- src/data/change-logs.ts
+++ src/data/change-logs.ts
@@ -34,11 +34,24 @@ const groupName = {
 export const changelog : PatchNote[] = [
   {
     version: '2.0.1',
-    date: '???',
+    date: '2024-09-10',
     changes: [
       {
         name: groupName.bugfix,
-        changes: []
+        changes: [
+          t('修复了客户端的安装更新按钮在已是最新版本时仍旧可以点击的问题。'),
+          t('修正了部分道具的中文译名。')
+        ]
+      },
+      {
+        name: groupName.feature,
+        changes: [
+          t('现在可以在“偏好设置”中自定义点击物品按钮时的行为。'),
+          t('重制了移动端“已选部件”弹窗的显示效果。'),
+          t('根据国服7.0特设站公布的新消息更新了职业名。'),
+          t('优化了部分场景下物品按钮悬浮窗的显示效果。'),
+          t('更新了“关于本作”中创作者的信息。')
+        ]
       }
     ]
   }
