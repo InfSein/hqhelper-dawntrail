3575df55e8b3913342f06eb402d03a1d53826f76
InfSein <infsein@foxmail.com>
2024-09-24 20:40:20 +0800

    feat: 客户端现在在下载HqHelper更新时会显示下载进度和速度 (仅限v3及更高版本的客户端)

--- env.electron.d.ts
+++ env.electron.d.ts
@@ -11,8 +11,18 @@ export interface ElectronAPI {
   httpGet: (url: string) => Promise<string>;
   simulatePing: (domain: string) => Promise<number | "error" | "timeout">;
   downloadUpdatePack: (url: string) => Promise<string>;
+  onUpdateProgress: (callback: (progressData: ProgressData) => void) => void;
   openUrlByBrowser: (url: string) => void;
   copyText: (text: string) => Promise<string>;
+}
+
+export interface ProgressData {
+  stage: "downloading" | "extracting" | "replacing" | "cleaning" | "relaunching"; // 当前阶段
+  progress?: {
+    total: number;      // 总下载大小 (MB)
+    downloaded: number; // 已下载大小 (MB)
+    speed: number;      // 下载速度 (MB/s)
+  };
 }
 
 // 将 `ElectronAPI` 类型暴露到全局 `window` 对象上
