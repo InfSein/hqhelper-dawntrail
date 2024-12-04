c2d5836b9a5c86dfd4a2e2f000179415dcb7f15d
InfSein <infsein@foxmail.com>
2024-12-04 20:25:40 +0800

    build: 升级依赖项 voerkai18n * 2.1.8 -> 2.1.13

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import i18nPlugin, { type VoerkaI18nPluginOptions } from '@voerkai18n/vue'
+import { i18nPlugin, type VoerkaI18nPluginOptions } from '@voerkai18n/vue'
 import { i18nScope } from './languages'
 
 import router from './router'
@@ -12,7 +12,7 @@ import { store, key } from './store'
 i18nScope.ready(() => {
   const app = createApp(App)
   app.use(router)
-  app.use<VoerkaI18nPluginOptions>(i18nPlugin, { i18nScope })
+  app.use<VoerkaI18nPluginOptions>(i18nPlugin as any, { i18nScope })
   app.use(store, key)
   app.mount('#app')
 })
