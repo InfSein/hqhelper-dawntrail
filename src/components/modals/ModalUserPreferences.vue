c3c711ca67eec4ee3e4338c9899782b3a185bb5c
InfSein <infsein@foxmail.com>
2024-12-11 17:19:43 +0800

    refactor: 抽离SettingGroup/SettingOption，精简代码

--- src/components/modals/ModalUserPreferences.vue
+++ src/components/modals/ModalUserPreferences.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import { inject, ref, type Ref } from 'vue'
 import {
-  NButton, NCascader, NCollapse, NCollapseItem, NIcon, NInput, NPopover, NRadioButton, NRadioGroup, NSelect, NSwitch, NTabs, NTabPane,
-  type CascaderOption
+  NButton, NCascader, NCollapse, NCollapseItem, NIcon, NInput, NPopover, NRadioButton, NRadioGroup, NSelect, NSwitch, NTabs, NTabPane
 } from 'naive-ui'
 import {
   HelpOutlineRound,
@@ -20,6 +19,7 @@ import MyModal from '../templates/MyModal.vue'
 import { useStore } from '@/store/index'
 import { type UserConfigModel, fixUserConfig } from '@/models/user-config'
 import { deepCopy } from '@/tools'
+import type { SettingGroup } from '@/models'
 
 const store = useStore()
 const t = inject<(text: string, ...args: any[]) => string>('t') ?? (() => { return '' })
@@ -29,29 +29,6 @@ const showModal = defineModel<boolean>('show', { required: true })
 const emit = defineEmits(['close', 'afterSubmit'])
 
 // #region data
-interface UserPreferenceGroup {
-  key: string
-  icon: typeof TravelExploreRound
-  text: string
-  children: UserPreferenceItem[]
-}
-interface UserPreferenceItem {
-  key: string
-  label: string
-  hide?: boolean
-  descriptions: {
-    value: string
-    class: string
-    style: string
-  }[]
-  warnings: {
-    value: string
-    class: string
-    style: string
-  }[]
-  type: 'radio-group' | 'switch' | 'select' | 'cascader' | 'string'
-  options: CascaderOption[]
-}
 const dealSimOptions = (options: string[]) => {
   return options.map(option => {
     return {
@@ -60,7 +37,16 @@ const dealSimOptions = (options: string[]) => {
     }
   })
 }
-const UserPreferenceGroups : UserPreferenceGroup[] = [
+const dealDescriptions = (descriptions: string[]) => {
+  return descriptions.map(description => {
+    return {
+      value: description,
+      class: '',
+      style: ''
+    }
+  })
+}
+const UserPreferenceGroups : SettingGroup[] = [
   /* General */
   {
     key: 'general',
@@ -70,8 +56,6 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
       {
         key: 'language_ui',
         label: t('界面语言'),
-        descriptions: [],
-        warnings: [],
         type: 'radio-group',
         options: [
           { value: 'zh', label: '简体中文' },
@@ -82,19 +66,10 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
       {
         key: 'language_item',
         label: t('物品语言'),
-        descriptions: [
-          {
-            value: t('选择程序中道具的语言。此设置还会影响一部分其他信息(例如地图名)的语言。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('如果选择“自动”，物品语言将跟随“界面语言”的设置。'),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [],
+        descriptions: dealDescriptions([
+          t('选择程序中道具的语言。此设置还会影响一部分其他信息(例如地图名)的语言。'),
+          t('如果选择“自动”，物品语言将跟随“界面语言”的设置。'),
+        ]),
         type: 'radio-group',
         options: [
           { value: 'auto', label: t('自动') },
@@ -106,19 +81,10 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
       {
         key: 'item_server',
         label: t('服务器'),
-        descriptions: [
-          {
-            value: t('选择您游戏账号所属的服务器。此设置还会影响一部分统计数据(例如点数道具的兑换价格)的计算方式。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('如果选择“自动”，程序会根据您在“界面语言”的设置自动判断。'),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [],
+        descriptions: dealDescriptions([
+          t('选择您游戏账号所属的服务器。此设置还会影响一部分统计数据(例如点数道具的兑换价格)的计算方式。'),
+          t('如果选择“自动”，程序会根据您在“界面语言”的设置自动判断。'),
+        ]),
         type: 'radio-group',
         options: [
           { value: 'auto', label: t('自动') },
@@ -137,8 +103,6 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
       {
         key: 'theme',
         label: t('主题'),
-        descriptions: [],
-        warnings: [],
         type: 'radio-group',
         options: [
           { value: 'system', label: t('跟随系统') },
@@ -149,24 +113,11 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
       {
         key: 'custom_font_size',
         label: t('字体大小'),
-        descriptions: [
-          {
-            value: t('全局性地修改程序/网站的字体大小。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('部分区域的字体大小不受此设置的影响。此外，还会受到浏览器“最小字号”设置的限制。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('暂时无法保证“标准”大小之外的显示效果。除非有特殊需要，否则不建议修改。'),
-            class: '',
-            style: ''
-          },
-        ],
-        warnings: [],
+        descriptions: dealDescriptions([
+          t('全局性地修改程序/网站的字体大小。'),
+          t('部分区域的字体大小不受此设置的影响。此外，还会受到浏览器“最小字号”设置的限制。'),
+          t('暂时无法保证“标准”大小之外的显示效果。除非有特殊需要，否则不建议修改。'),
+        ]),
         type: 'radio-group',
         options: [
           { value: '12px', label: t('更小') },
@@ -179,56 +130,23 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
       {
         key: 'custom_font',
         label: t('自定义字体'),
-        descriptions: [
-          {
-            value: t('全局性地修改程序/网站的字体。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('可以设置多个字体，用英文逗号分隔。当前一个字体字库未包括要显示的文字时，程序会使用下一个字体；如果均不包括，程序会使用设置前的字体。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('字体名称出现空格时，建议用英文的单引号或双引号来将其包裹，例如"思源黑体 CN Medium"。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('如果你对CSS有所了解，可以直接参照font-family的语法来填写。'),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [
-        ],
-        type: 'string',
-        options: []
+        descriptions: dealDescriptions([
+          t('全局性地修改程序/网站的字体。'),
+          t('可以设置多个字体，用英文逗号分隔。当前一个字体字库未包括要显示的文字时，程序会使用下一个字体；如果均不包括，程序会使用设置前的字体。'),
+          t('字体名称出现空格时，建议用英文的单引号或双引号来将其包裹，例如"思源黑体 CN Medium"。'),
+          t('如果你对CSS有所了解，可以直接参照font-family的语法来填写。'),
+        ]),
+        type: 'string'
       },
       {
         key: 'hide_collector_icons',
         label: t('隐藏物品按钮的职业图标'),
-        descriptions: [
-          {
-            value: t('部分物品按钮会在物品名处展示该物品对应的生产/采集职业图标。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('虽然会更方便，但显示效果可能就不太尽如人意。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('如果你觉得这样太过碍眼，请考虑打开此选项。'),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [],
-        type: 'switch',
-        options: []
+        descriptions: dealDescriptions([
+          t('部分物品按钮会在物品名处展示该物品对应的生产/采集职业图标。'),
+          t('虽然会更方便，但显示效果可能就不太尽如人意。'),
+          t('如果你觉得这样太过碍眼，请考虑打开此选项。'),
+        ]),
+        type: 'switch'
       },
     ]
   },
@@ -242,104 +160,52 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
         key: 'disable_patchcard_autofold',
         label: t('禁用选择版本后自动折叠'),
         hide: !isMobile.value,
-        descriptions: [
-          {
-            value: t('"选择版本"在手机端有些太占地方，而且一般也不会被高频率地修改，所以我们默认在你选择版本之后就自动折叠它。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('如果你觉得不自动折叠会更好，请打开此选项。'),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [],
-        type: 'switch',
-        options: []
+        descriptions: dealDescriptions([
+          t('"选择版本"在手机端有些太占地方，而且一般也不会被高频率地修改，所以我们默认在你选择版本之后就自动折叠它。'),
+          t('如果你觉得不自动折叠会更好，请打开此选项。'),
+        ]),
+        type: 'switch'
       },
       {
         key: 'disable_jobbtn_doubleclick',
         label: t('禁用重复点击已选择的职业按钮时添加主副手'),
-        descriptions: [
-          {
-            value: t('在选择了职业之后再度点击职业按钮，将会默认添加一套主副手武器或工具。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('这一功能在第一代HqHelper是默认关闭的，而本代默认开启。如果这与您的习惯相悖，请打开此选项。'),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [],
-        type: 'switch',
-        options: []
+        descriptions: dealDescriptions([
+          t('在选择了职业之后再度点击职业按钮，将会默认添加一套主副手武器或工具。'),
+          t('这一功能在第一代HqHelper是默认关闭的，而本代默认开启。如果这与您的习惯相悖，请打开此选项。'),
+        ]),
+        type: 'switch'
       },
       {
         key: 'use_traditional_statement',
         label: t('使用旧版本制作报表'),
-        descriptions: [
-          {
-            value: t('在2.0.10版本，我们添加了专业版制作报表，提供更详细的物品信息表格，并支持根据已准备素材计算尚需素材。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('如果你并不需要这些功能，或是更喜欢旧版本制作报表的风格，可以考虑打开此选项。'),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [],
-        type: 'switch',
-        options: []
+        descriptions: dealDescriptions([
+          t('在2.0.10版本，我们添加了专业版制作报表，提供更详细的物品信息表格，并支持根据已准备素材计算尚需素材。'),
+          t('如果你并不需要这些功能，或是更喜欢旧版本制作报表的风格，可以考虑打开此选项。'),
+        ]),
+        type: 'switch'
       },
       {
         key: 'click_to_show_pop_in_span',
         label: t('手动控制二级悬浮窗'),
         hide: isMobile.value,
-        descriptions: [
-          {
-            value: t('在物品按钮悬浮窗内可能有一些可以打开子悬浮窗的元素，比如制作素材和地图按钮。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('在默认情况下，光标悬停在元素上时就会立即打开子悬浮窗。如果你觉得这样太容易误触，可以打开此选项，只通过左键单击来控制子悬浮窗的显示与否。'),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [],
-        type: 'switch',
-        options: []
+        descriptions: dealDescriptions([
+          t('在物品按钮悬浮窗内可能有一些可以打开子悬浮窗的元素，比如制作素材和地图按钮。'),
+          t('在默认情况下，光标悬停在元素上时就会立即打开子悬浮窗。如果你觉得这样太容易误触，可以打开此选项，只通过左键单击来控制子悬浮窗的显示与否。'),
+        ]),
+        type: 'switch'
       },
       {
         key: 'macro_direct_copy',
         label: t('点击“复制宏”时直接复制'),
-        descriptions: [
-          {
-            value: t('在默认情况下，每次您点击“复制宏”按钮，程序都会弹窗询问您要复制哪种宏。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('如果您希望提升效率，可以启用此选项，让程序直接按照默认的宏前缀来复制宏。'),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [],
-        type: 'switch',
-        options: []
+        descriptions: dealDescriptions([
+          t('在默认情况下，每次您点击“复制宏”按钮，程序都会弹窗询问您要复制哪种宏。'),
+          t('如果您希望提升效率，可以启用此选项，让程序直接按照默认的宏前缀来复制宏。'),
+        ]),
+        type: 'switch'
       },
       {
         key: 'macro_copy_prefix',
         label: t('默认宏前缀'),
-        descriptions: [],
-        warnings: [],
         type: 'select',
         options: [
           { value: '', label: t('直接复制(无前缀)') },
@@ -352,8 +218,6 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
       {
         key: 'item_button_click_event',
         label: t('点击物品按钮时的行为'),
-        descriptions: [],
-        warnings: [],
         type: 'select',
         options: [
           { value: 'none', label: t('什么都不做') },
@@ -364,19 +228,10 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
       {
         key: 'item_info_icon_click_event',
         label: t('点击物品信息图标时的行为'),
-        descriptions: [
-          {
-            value: t('控制点击部分区域物品名称右侧蓝色信息图标时触发的事件。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('“{option}”与此项目的设置可能会有冲突。', t('手动控制二级悬浮窗')),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [],
+        descriptions: dealDescriptions([
+          t('控制点击部分区域物品名称右侧蓝色信息图标时触发的事件。'),
+          t('“{option}”与此项目的设置可能会有冲突。', t('手动控制二级悬浮窗')),
+        ]),
         type: 'select',
         options: [
           { value: 'none', label: t('什么都不做') },
@@ -387,19 +242,10 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
       {
         key: 'item_list_style',
         label: t('材料清单格式'),
-        descriptions: [
-          {
-            value: t('决定在点击材料区域物品按钮组上方的“清单”按钮后，展示的物品清单格式。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('部分程序可能可以识别并导入特定格式的清单。'),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [],
+        descriptions: dealDescriptions([
+          t('决定在点击材料区域物品按钮组上方的“清单”按钮后，展示的物品清单格式。'),
+          t('部分程序可能可以识别并导入特定格式的清单。'),
+        ]),
         type: 'select',
         options: [
           { value: 'standard', label: t('标准 (物品名称 x 数量)') },
@@ -418,18 +264,10 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
       {
         key: 'disable_workstate_cache',
         label: t('禁用工作状态记忆'),
-        descriptions: [
-          {
-            value: t('应用默认记录您的版本、职业和部件选择，从而让应用长期保持上次关闭时的的工作状态。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('如果您希望每次打开应用都从头开始，或是在使用过程中出现较严重的卡顿，则可以考虑打开此选项。'),
-            class: '',
-            style: ''
-          }
-        ],
+        descriptions: dealDescriptions([
+          t('应用默认记录您的版本、职业和部件选择，从而让应用长期保持上次关闭时的的工作状态。'),
+          t('如果您希望每次打开应用都从头开始，或是在使用过程中出现较严重的卡顿，则可以考虑打开此选项。'),
+        ]),
         warnings: [
           {
             value: t('注意：禁用工作状态记忆后，已记录的工作状态将被立即删除！'),
@@ -437,8 +275,7 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
             style: ''
           }
         ],
-        type: 'switch',
-        options: []
+        type: 'switch'
       }
     ]
   },
@@ -461,19 +298,10 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
       {
         key: 'universalis_server',
         label: t('物品价格服务器'),
-        descriptions: [
-          {
-            value: t('适用于“{f}”功能。', t('成本/收益预估')),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('下方的输入框支持通过输入关键词来检索选项。'),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [],
+        descriptions: dealDescriptions([
+          t('适用于“{f}”功能。', t('成本/收益预估')),
+          t('下方的输入框支持通过输入关键词来检索选项。'),
+        ]),
         type: 'cascader',
         options: [
         {
@@ -630,38 +458,21 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
         key: 'universalis_priceType',
         label: t('物品价格类型'),
         descriptions: [
-          {
-            value: t('适用于“{f}”功能。', t('成本/收益预估')),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('计算成本时，默认计算制作材料NQ的价格；计算收益时，默认计算成品道具HQ的价格。'),
-            class: '',
-            style: ''
-          },
+          ...dealDescriptions([
+            t('适用于“{f}”功能。', t('成本/收益预估')),
+            t('计算成本时，默认计算制作材料NQ的价格；计算收益时，默认计算成品道具HQ的价格。'),
+          ]),
           {
             value: t('部分选项说明：'),
             class: '',
             style: 'margin-top: 5px;'
           },
-          {
-            value: t('近期成交价格：选取最近5条成交记录计算平均价格。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('当前寄售最低价：在交易板前10条在售记录中选取最低价格。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('当前寄售平均价：选取交易板前10条在售记录计算平均价格。'),
-            class: '',
-            style: ''
-          }
+          ...dealDescriptions([
+            t('近期成交价格：选取最近5条成交记录计算平均价格。'),
+            t('当前寄售最低价：在交易板前10条在售记录中选取最低价格。'),
+            t('当前寄售平均价：选取交易板前10条在售记录计算平均价格。'),
+          ]),
         ],
-        warnings: [],
         type: 'select',
         options: [
           {
@@ -697,24 +508,11 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
       {
         key: 'universalis_expireTime',
         label: t('物品价格有效期'),
-        descriptions: [
-          {
-            value: t('适用于“{f}”功能。', t('成本/收益预估')),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('程序会将获取到的物品价格缓存。一旦超出设置的有效期，就需要重新获取价格信息。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('设定时间过短，会导致计算价格的效率降低；设置时间过长，则会导致结果过时。'),
-            class: '',
-            style: ''
-          }
-        ],
-        warnings: [],
+        descriptions: dealDescriptions([
+          t('适用于“{f}”功能。', t('成本/收益预估')),
+          t('程序会将获取到的物品价格缓存。一旦超出设置的有效期，就需要重新获取价格信息。'),
+          t('设定时间过短，会导致计算价格的效率降低；设置时间过长，则会导致结果过时。'),
+        ]),
         type: 'select',
         options: [
           {
@@ -747,10 +545,7 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
         key: 'enable_dev_mode',
         label: t('启用开发者模式'),
         hide: !window.electronAPI?.openDevTools,
-        descriptions: [],
-        warnings: [],
-        type: 'switch',
-        options: []
+        type: 'switch'
       }
     ]
   },
@@ -764,25 +559,17 @@ const UserPreferenceGroups : UserPreferenceGroup[] = [
         key: 'disable_auto_update',
         label: t('禁用自动更新'),
         descriptions: [
-          {
-            value: t('我们默认在您启动应用时检查一次最新版本，并提示您进行更新。'),
-            class: '',
-            style: ''
-          },
-          {
-            value: t('如果您不希望接收到更新提示，则可以考虑打开此选项。'),
-            class: '',
-            style: ''
-          },
+          ...dealDescriptions([
+            t('我们默认在您启动应用时检查一次最新版本，并提示您进行更新。'),
+            t('如果您不希望接收到更新提示，则可以考虑打开此选项。'),
+          ]),
           {
             value: t('在使用网页端时，即使打开了这一选项，浏览器也仍会因为缓存而自动更新。'),
             class: 'color-info',
             style: ''
           }
         ],
-        warnings: [],
-        type: 'switch',
-        options: []
+        type: 'switch'
       }
     ]
   }
@@ -872,7 +659,7 @@ const handleSave = () => {
                   <div class="item-title">{{ item.label }}</div>
                 </template>
                 <template #arrow>
-                  <n-icon v-if="item.descriptions.length" :title="t('点击以展开或折叠此设置项的描述')"><HelpOutlineRound /></n-icon>
+                  <n-icon v-if="item.descriptions?.length" :title="t('点击以展开或折叠此设置项的描述')"><HelpOutlineRound /></n-icon>
                   <n-icon v-else></n-icon>
                 </template>
 
@@ -890,7 +677,7 @@ const handleSave = () => {
             </n-collapse>
             <div class="item-input">
               <n-popover
-                v-if="item.warnings.length"
+                v-if="item.warnings?.length"
                 :trigger="isMobile ? 'click' : 'hover'"
                 placement="bottom"
                 :style="item.type === 'switch' ? 'max-width: 300px;' : ''"
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,33 @@
+import type { Component } from "vue"
+import type { CascaderOption } from "naive-ui"
+
 export interface AppVersionJson {
   hqhelper: string;
   electron: string;
   dlink_hqhelper: string;
   dlink_electron: string;
+}
+
+export interface SettingGroup {
+  key: string
+  icon: Component
+  text: string
+  children: SettingOption[]
+}
+export interface SettingOption {
+  key: string
+  label: string
+  hide?: boolean
+  descriptions?: {
+    value: string
+    class: string
+    style: string
+  }[]
+  warnings?: {
+    value: string
+    class: string
+    style: string
+  }[]
+  type: 'radio-group' | 'switch' | 'select' | 'cascader' | 'string'
+  options?: CascaderOption[]
 }
