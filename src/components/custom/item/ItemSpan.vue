b7b671f864990b8478216bbe88db3ffaed03b701
InfSein <infsein@foxmail.com>
2025-04-29 23:51:51 +0800

    temp: 进一步优化溢出算法；信息图标追加光标指针

--- src/components/custom/item/ItemSpan.vue
+++ src/components/custom/item/ItemSpan.vue
@@ -135,8 +135,9 @@ const popTrigger = computed(() => {
 const containerStyle = computed(() => {
   let itemAmountwidth = 0
   if (props.showAmount && !!itemAmountNode.value) {
-    const itemAmountWidth = itemAmountNode.value.getBoundingClientRect().width
-    itemAmountwidth = Math.ceil(itemAmountWidth) + 8
+    const itemAmountWidth = itemAmountNode.value.offsetWidth
+    const offset = itemLanguage.value === 'zh' ? 3 : 6 
+    itemAmountwidth = Math.ceil(itemAmountWidth) + offset
   }
   return [
     (props.containerStyle ?? ''),
@@ -167,7 +168,7 @@ const handleItemIconClick = async () => {
   <div class="container" :style="containerStyle">
     <XivFARImage
       v-show="!hideIcon"
-      class="img no-select"
+      class="no-select"
       :size="imgSize ?? 14"
       :src="itemInfo.iconUrl"
       :title="(hideName && hidePopIcon) ? getItemName() : ''"
@@ -188,7 +189,7 @@ const handleItemIconClick = async () => {
       :pop-custom-width="275"
       :pop-trigger="popTrigger"
     >
-      <n-icon v-if="!hidePopIcon" class="pop-icon" size="14" color="#3b7fef"
+      <n-icon v-if="!hidePopIcon" class="item-popicon" size="14" color="#3b7fef"
         @contextmenu="handleContextMenu"
         @touchstart.passive="handleItemButtonTouchStart" 
         @touchmove.passive="handleItemButtonTouchMove" 
@@ -237,6 +238,10 @@ const handleItemIconClick = async () => {
       vertical-align: top;
       white-space: nowrap;
     }
+  }
+
+  .item-popicon {
+    cursor: pointer;
   }
 }
 </style>
