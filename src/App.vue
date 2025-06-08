ea13419286dae647fb5ada1fbdfbe61da478539b
InfSein <infsein@foxmail.com>
2025-06-08 22:17:59 +0800

    refactor: 修正全局 appClass 的挂载方式，使其能对后期组件生效

--- src/App.vue
+++ src/App.vue
@@ -147,6 +147,7 @@ provide('displayCheckUpdatesModal', displayCheckUpdatesModal)
 const appClass = computed(() => {
   const classes = [
     'lang-' + locale.value,
+    'theme-' + theme.value,
     'app-' + (isMobile.value ? 'mobile' : 'desktop'),
     window.electronAPI ? 'env-electron' : 'env-web',
     appMode.value === 'overlay' ? 'env-overlay' : ''
@@ -255,6 +256,7 @@ const naiveUIThemeOverrides = computed(() : GlobalThemeOverrides => {
     :theme="naiveUiTheme"
     :locale="naiveUiLocale"
     :date-locale="naiveUiDateLocale"
+    :namespace="appClass"
     :theme-overrides="naiveUIThemeOverrides"
   >
     <n-global-style />
