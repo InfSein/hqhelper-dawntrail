f5f77293fcb2464098122747cc6a8646096aba4b
InfSein <infsein@foxmail.com>
2025-08-08 15:05:38 +0800

    feat: 优化了7.3版本主界面灵砂统计的样式

--- src/components/main/StatisticsPanel.vue
+++ src/components/main/StatisticsPanel.vue
@@ -346,7 +346,7 @@ const handleAnalysisItemPrices = async () => {
           <ItemList
             :items="aethersands"
             :list-height="isMobile ? undefined : 120"
-            display-style="display: grid; grid-template-columns: repeat(2, minmax(0, 1fr));"
+            :display-style="aethersands.length > 1 ? 'display: grid; grid-template-columns: repeat(2, minmax(0, 1fr));' : ''"
             scroll-style="overflow-y: auto;"
             btn-pop-use-custom-width
             :btn-pop-custom-width="isMobile ? 300 : undefined"
