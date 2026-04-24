46e420647602de36274462e211e8444f61ec7255
Inf Sein <54071139+InfSein@users.noreply.github.com>
2026-04-25 00:03:16 +0800

    feat: 宏管理增加多选-批量操作功能 (#228)

--- src/views/MacroManagePage.vue
+++ src/views/MacroManagePage.vue
@@ -10,6 +10,7 @@ import {
   ArchiveSharp, UnarchiveSharp,
   AddTaskOutlined, PlaylistAddCheckOutlined,
   ShareOutlined, EditNoteOutlined, DeleteFilled,
+  ChecklistOutlined,
 } from '@vicons/material'
 import { compress, decompress } from 'xiv-cac-utils'
 import ItemSpan from '@/components/custom/item/ItemSpan.vue'
@@ -74,7 +75,7 @@ watch(workState, async () => {
   } else {
     console.warn('workState or userConfig is not defined')
   }
-}, {deep: true})
+}, { deep: true })
 
 const tableHeight = ref(300)
 const updateHeights = () => {
@@ -113,7 +114,7 @@ const handleUrlParamsImport = () => {
     macroEditAction.value = 'add'
     showModalCraftMacroEdit.value = true
 
-    function getMacroId () {
+    function getMacroId() {
       let macroid = workState.value.recordIndex
       while (workState.value.recordedCraftMacros.find(macro => macro.id === macroid)) {
         macroid++
@@ -151,6 +152,11 @@ const multiOperateDropdownOptions = computed(() => {
       label: t('macro_manage.text.batch_add'),
       key: 'batch-add',
       icon: renderIcon(PlaylistAddCheckOutlined),
+    },
+    {
+      label: t('macro_manage.text.enable_select_mode'),
+      key: 'select',
+      icon: renderIcon(ChecklistOutlined),
     },
     {
       label: t('macro_manage.text.delete_all_macros'),
@@ -316,7 +322,10 @@ const tableColumns = computed(() => {
             ),
             h(
               'div',
-              { class: 'flex-vac gap-2' },
+              {
+                class: 'flex-vac gap-2',
+                style: selectMode.value ? 'visibility: hidden;' : ''
+              },
               macros.map((macro, index) => {
                 return h(
                   NButton,
@@ -343,7 +352,10 @@ const tableColumns = computed(() => {
       render(row) {
         return h(
           'div',
-          { class: 'flex gap-4' },
+          {
+            class: 'flex gap-4',
+            style: selectMode.value ? 'visibility: hidden;' : ''
+          },
           [
             h(
               NButton,
@@ -418,10 +430,97 @@ const handleMultiOperateDropdownSelect = async (key: string | number) => {
     }
     workState.value.recordedCraftMacros = []
     NAIVE_UI_MESSAGE.success(t('common.message.deleted'))
+  } else if (key === 'select') {
+    handleSelectModeEnter()
   } else {
     console.warn('unexpected multi operate dropdown key:', key)
   }
 }
+
+const selectMode = ref(false)
+const checkedRowKeys = ref<number[]>([])
+
+const handleSelectModeEnter = () => {
+  selectMode.value = true
+  checkedRowKeys.value = []
+}
+
+const handleSelectModeExit = () => {
+  selectMode.value = false
+  checkedRowKeys.value = []
+}
+
+const handleSelectAll = () => {
+  checkedRowKeys.value = tableData.value.map(row => row.id)
+}
+
+const handleInvertSelection = () => {
+  const allIds = tableData.value.map(row => row.id)
+  checkedRowKeys.value = allIds.filter(id => !checkedRowKeys.value.includes(id))
+}
+
+const handleShareSelected = async () => {
+  if (!checkedRowKeys.value.length) {
+    NAIVE_UI_MESSAGE.warning('请先选择要分享的宏')
+    return
+  }
+  const lines = checkedRowKeys.value.map(id => {
+    const macro = workState.value.recordedCraftMacros.find(m => m.id === id)
+    if (!macro) return ''
+    const row = unarchiveMacroRow(macro)
+    const cac = compress({
+      type: 'id',
+      actions: macro.craftActions,
+    })
+    return `${cac} ${row.name} ${row.requirements.craftsmanship || 0} ${row.requirements.control || 0} ${row.requirements.cp || 0}`
+  }).filter(Boolean)
+
+  const text = lines.join('\n')
+  const response = await CopyToClipboard(text)
+  if (response) {
+    NAIVE_UI_MESSAGE.error(t('common.message.copy_failed_unexpected_error'))
+  } else {
+    NAIVE_UI_MESSAGE.success(t('common.message.copy_succeed'))
+  }
+}
+
+const handleDeleteSelected = async () => {
+  if (!checkedRowKeys.value.length) {
+    NAIVE_UI_MESSAGE.warning('请先选择要删除的宏')
+    return
+  }
+  if (!await confirmWarning(`确定要删除选中的 ${checkedRowKeys.value.length} 个项目吗？\n${t('common.message.operation_irreversible')}`)) {
+    return
+  }
+  workState.value.recordedCraftMacros = workState.value.recordedCraftMacros.filter(
+    macro => !checkedRowKeys.value.includes(macro.id)
+  )
+  NAIVE_UI_MESSAGE.success(t('common.message.deleted'))
+}
+
+const rowProps = (row: CraftMacroRow) => {
+  return {
+    style: selectMode.value ? 'cursor: pointer;' : '',
+    onClick: () => {
+      if (selectMode.value) {
+        const index = checkedRowKeys.value.indexOf(row.id)
+        if (index > -1) {
+          checkedRowKeys.value.splice(index, 1)
+        } else {
+          checkedRowKeys.value.push(row.id)
+        }
+      }
+    }
+  }
+}
+
+const rowClassName = (row: CraftMacroRow) => {
+  if (selectMode.value && checkedRowKeys.value.includes(row.id)) {
+    return 'selected-row'
+  }
+  return ''
+}
+
 const handleImportButtonClick = () => {
   imexportMode.value = 'import'
   showModalImExport.value = true
@@ -441,7 +540,7 @@ const handleAddRow = () => {
   macroEditAction.value = 'add'
   showModalCraftMacroEdit.value = true
 
-  function getMacroId () {
+  function getMacroId() {
     let macroid = workState.value.recordIndex
     while (workState.value.recordedCraftMacros.find(macro => macro.id === macroid)) {
       macroid++
@@ -486,7 +585,7 @@ const handleEditRow = (row: CraftMacroRow) => {
   }
 }
 const handleDeleteRow = async (row: CraftMacroRow) => {
-  if (!await confirmWarning(t('common.message.confirm_delete_with_name', row.name) + '\n' + t('common.message.operation_irreversible'))){
+  if (!await confirmWarning(t('common.message.confirm_delete_with_name', row.name) + '\n' + t('common.message.operation_irreversible'))) {
     return
   }
   const index = workState.value.recordedCraftMacros.findIndex(macro => macro.id === row.id)
@@ -582,41 +681,59 @@ const handleSettingButtonClick = () => {
             />
           </n-input-group>
           <div id="querier-actions">
-            <n-dropdown
-              placement="bottom-end"
-              :options="multiOperateDropdownOptions"
-              @select="handleMultiOperateDropdownSelect"
-            >
-              <n-button ghost icon-placement="right">
+            <template v-if="!selectMode">
+              <n-dropdown
+                placement="bottom-end"
+                :options="multiOperateDropdownOptions"
+                @select="handleMultiOperateDropdownSelect"
+              >
+                <n-button ghost icon-placement="right">
+                  <template #icon>
+                    <n-icon :component="KeyboardArrowDownRound" />
+                  </template>
+                  {{ t('macro_manage.text.batch_operate') }}
+                </n-button>
+              </n-dropdown>
+              <n-button-group>
+                <n-button ghost @click="handleExportButtonClick">
+                  <template #icon>
+                    <n-icon :component="UnarchiveSharp" />
+                  </template>
+                  {{ t('common.export') }}
+                </n-button>
+                <n-button ghost @click="handleImportButtonClick">
+                  <template #icon>
+                    <n-icon :component="ArchiveSharp" />
+                  </template>
+                  {{ t('common.import') }}
+                </n-button>
+              </n-button-group>
+              <n-button type="primary" @click="handleAddRow">
                 <template #icon>
-                  <n-icon :component="KeyboardArrowDownRound" />
+                  <n-icon :component="AddTaskOutlined" />
                 </template>
-                {{ t('macro_manage.text.batch_operate') }}
+                {{ t('common.add2') }}
               </n-button>
-            </n-dropdown>
-            <n-button-group>
-              <n-button ghost @click="handleExportButtonClick">
-                <template #icon>
-                  <n-icon :component="UnarchiveSharp" />
-                </template>
-                {{ t('common.export') }}
+            </template>
+            <template v-else>
+              <n-button-group>
+                <n-button ghost @click="handleSelectAll">{{ t('common.select_all') }}</n-button>
+                <n-button ghost @click="handleInvertSelection">{{ t('common.select_invert') }}</n-button>
+              </n-button-group>
+              <n-button-group>
+                <n-button ghost @click="handleShareSelected">
+                  <template #icon><n-icon :component="ShareOutlined" /></template>
+                  {{ t('common.share') }}
+                </n-button>
+                <n-button ghost type="error" @click="handleDeleteSelected">
+                  <template #icon><n-icon :component="DeleteFilled" /></template>
+                  {{ t('common.delete') }}
+                </n-button>
+              </n-button-group>
+              <n-button ghost @click="handleSelectModeExit">
+                {{ t('common.exit_selection_mode') }}
               </n-button>
-              <n-button ghost @click="handleImportButtonClick">
-                <template #icon>
-                  <n-icon :component="ArchiveSharp" />
-                </template>
-                {{ t('common.import') }}
-              </n-button>
-            </n-button-group>
-            <n-button
-              type="primary"
-              @click="handleAddRow"
-            >
-              <template #icon>
-                <n-icon :component="AddTaskOutlined" />
-              </template>
-              {{ t('common.add2') }}
-            </n-button>
+            </template>
           </div>
         </div>
         <n-data-table
@@ -624,6 +741,8 @@ const handleSettingButtonClick = () => {
           scroll-x="100%"
           :columns="tableColumns"
           :data="tableData"
+          :row-props="rowProps"
+          :row-class-name="rowClassName"
           :pagination="false"
           :max-height="tableHeight"
           :virtual-scroll="tableData.length > 200"
@@ -636,7 +755,7 @@ const handleSettingButtonClick = () => {
         </n-data-table>
       </div>
     </FoldableCard>
-    
+
     <n-back-top />
 
     <ModalCraftMacroEdit
@@ -665,6 +784,9 @@ const handleSettingButtonClick = () => {
 </template>
 
 <style scoped>
+:deep(.selected-row > td) {
+  background-color: var(--n-td-color-hover) !important;
+}
 #main-container {
   max-width: 100%;
   gap: 0.6rem;
@@ -726,7 +848,7 @@ const handleSettingButtonClick = () => {
 @media screen and (max-width: 767px) {
   .query-options {
     flex-direction: column;
-    
+
     #querier-search {
       width: 100%;
     }
